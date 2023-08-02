71278a68e6d017a202168de5235df5b75df97a68
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-08-02 20:03:22 +0200

    update

--- Exemplo-001/script.js
+++ Exemplo-001/script.js
@@ -1,16 +1,11 @@
 function saudar() {
     const nome = document.getElementById("nome").value;
+    const idade = document.getElementById("idade").value;
 
     if (nome === "") {
-      alert("Por favor, digite seu nome antes de saudar.");
+      alert("Por favor, digite seu nome e idade antes de saudar.");
     } else {
-      const mensagem = "Olá, " + nome + "! Bem-vindo!";
+      const mensagem = "Olá, " + nome + "! Bem-vindo!, vejo que possui" + idade + "anos";
       document.getElementById("mensagem").innerText = mensagem;
     }
-  }
-
-  function mostraIdade() {
-    const idade = document.getElementById("idade").value;
-    const info = "Sua idade é: " + idade;
-    document.getElementById("info").innerText = info;
   }
