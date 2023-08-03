70f548f8ded896b58379a37e284b9772766a969b
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-08-03 10:47:56 +0200

    Ajustes

--- Exemplo-001/script.js
+++ Exemplo-001/script.js
@@ -1,11 +1,17 @@
 function saudar() {
-    const nome = document.getElementById("nome").value;
-    const idade = document.getElementById("idade").value;
+  const nome = document.getElementById("nome").value;
+  const idade = document.getElementById("idade").value;
 
-    if (nome === "") {
-      alert("Por favor, digite seu nome e idade antes de saudar.");
-    } else {
-      const mensagem = "Olá, " + nome + "! Bem-vindo!, vejo que possui" + idade + "anos";
-      document.getElementById("mensagem").innerText = mensagem;
-    }
+  if (nome === "") {
+    // Mostra o modal quando o botão "Saudar" é clicado, mas o campo de nome está vazio
+    document.getElementById('custom-modal').style.display = 'block';
+  } else {
+    const mensagem = "Olá, " + nome + "! Bem-vindo!, vejo que possui " + idade + " anos";
+    document.getElementById("mensagem").innerText = mensagem;
   }
+}
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+  // Oculta o modal quando o botão de fechar no modal é clicado
+  document.getElementById('custom-modal').style.display = 'none';
+});
