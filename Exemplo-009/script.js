8c5f7239071eefaacc04222b60cf36449b7ce315
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-08-06 10:10:08 +0200

    Updates

---
+++ Exemplo-009/script.js
@@ -0,0 +1,11 @@
+console.log('teste');
+
+let idade = 33;
+console.log(idade);
+
+
+let altura = 1.70;
+console.log(altura);
+
+let sexo = ("Masculino");
+console.log(sexo);
---
+++ Exemplo-010/script.js
@@ -0,0 +1,15 @@
+const valorIngreco = 20;
+//const não muda valor
+console.log(valorIngreco);
+
+
+//TIPOS PRIMIRIVOS
+let nome = 'Odair'; // stirng literal
+let idade = 33; // number literal
+let numero = 25.4; //number literal
+let estaAprovado = true; // boolean  ajuda a construir regras no aplicativo.
+
+let sobrenome = undefined; //undefined
+let corSelecionada = null; //casos onde você queira resetar o valor. Refefinir um valor.
+
+//TIPAGEM DINÂMICA
